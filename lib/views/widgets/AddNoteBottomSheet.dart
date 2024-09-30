0e172ca1dbf8ec3db7c401ae50d55fe7ee4ad05f
Rajaatawe <rajaataw0@gmail.com>
2024-09-30 15:00:41 +0300

    create Textfield

--- lib/views/widgets/AddNoteBottomSheet.dart
+++ lib/views/widgets/AddNoteBottomSheet.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/TextField.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column
+    (
+      children: [
+        Textfield()
+      ],
+    );
   }
 }
---
+++ lib/views/widgets/TextField.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Textfield extends StatelessWidget {
+  const Textfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: )
+      ),
+    );
+  }
+}
