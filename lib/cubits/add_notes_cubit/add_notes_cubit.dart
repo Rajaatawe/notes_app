60f8fd5166ffd34c77ed0b35564e6cb45ea5d790
Rajaatawe <rajaataw0@gmail.com>
2024-10-02 14:56:58 +0300

    provide add note cubit

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,8 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:hive/hive.dart';
-import 'package:meta/meta.dart';
 import 'package:note_app/constants.dart';
-import 'package:note_app/main.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'add_notes_state.dart';
@@ -10,15 +8,14 @@ part 'add_notes_state.dart';
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
-  addNotes(NoteModel note) async{
+  addNotes(NoteModel note) async {
     emit(AddNotesLoading());
     try {
-  var notesBox = Hive.box<NoteModel>(kNotesBox);
-  emit(AddNotesSuccess());
-     await notesBox.add(note);
-} on Exception catch (e) {
-  AddNotesFailure(errorMessage: 'Error adding notes $e.toString()');
-}
-
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNotesSuccess());
+      await notesBox.add(note);
+    } on Exception catch (e) {
+      AddNotesFailure(errorMessage: 'Error adding notes $e.toString()');
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/notes_view.dart';
 
@@ -11,19 +13,27 @@ void main() async {
   runApp(Notes());
 }
 
-
 class Notes extends StatelessWidget {
   const Notes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        //  fontFamily:'lib/assets/fonts/fa-regular-400.ttf' ,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const NotesView(),
-    );
-  }
+    return MultiBlocProvider(
+      //+
+      providers: [
+        //+
+        BlocProvider(create: (context) => AddNotesCubit()), //+
+      ], //+
+      child: MaterialApp(
+        //+
+        theme: ThemeData(
+          //+
+          brightness: Brightness.dark, //+
+          //  fontFamily:'lib/assets/fonts/fa-regular-400.ttf' ,//+
+        ), //+
+        debugShowCheckedModeBanner: false, //+
+        home: const NotesView(), //+
+      ), //+
+    ); //+/-
+  } //-
 }
