941535975d4a81b76cc838965252ac135a252dce
Rajaatawe <rajaataw0@gmail.com>
2024-10-07 13:56:37 +0300

    delete note done

--- lib/views/widgets/Note_Item.dart
+++ lib/views/widgets/Note_Item.dart
@@ -8,38 +8,58 @@ class NoteItem extends StatelessWidget {
   final NoteModel note;
   @override
   Widget build(BuildContext context) {
-    return  GestureDetector(
-      onTap: (){
-        Navigator.push(context,MaterialPageRoute(builder: (context){
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
           return EditNoteView();
-        }) );
+        }));
       },
       child: Padding(
         padding: const EdgeInsets.only(top: 10),
         child: Container(
-        decoration: BoxDecoration(
-          color: Color(note.color),
-          borderRadius: BorderRadius.circular(16 ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-          ListTile(
-            title: Text(note.title,style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 26),),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16 ),
-              child: Text(note.subTitle,style:TextStyle(color: Colors.black.withOpacity(0.5),fontSize: 18)),
-            ),
-            trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,color: Colors.black,size: 30,)),
+          decoration: BoxDecoration(
+            color: Color(note.color),
+            borderRadius: BorderRadius.circular(16),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24,bottom: 12),
-            child: Text(note.date,style: TextStyle(color: Colors.black.withOpacity(0.4),fontSize: 16),),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  note.title,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: Text(note.subTitle,
+                      style: TextStyle(
+                          color: Colors.black.withOpacity(0.5), fontSize: 18)),
+                ),
+                trailing: IconButton(
+                    onPressed: () {
+                      note.delete();
+                    },
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.black,
+                      size: 30,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 24, bottom: 12),
+                child: Text(
+                  note.date,
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.4), fontSize: 16),
+                ),
+              ),
+            ],
           ),
-        ],),
         ),
       ),
     );
   }
 }
-  
